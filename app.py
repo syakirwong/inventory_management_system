43b7cbfc35f531f64d84a3c5b0c62a58ae1795be
Syakir Wong <mohdsyakir9997@gmail.com>
2021-01-11 13:51:48 +0800

    added database inventory_management_dev

--- app.py
+++ app.py
@@ -1,5 +1,21 @@
+import peeweedbevolve # new; must be imported before models
 from flask import Flask, render_template, request
+from models import db
+
 app = Flask(__name__)
+
+@app.before_request
+def before_request():
+   db.connect()
+
+@app.after_request
+def after_request(response):
+   db.close()
+   return response
+
+@app.cli.command() # new
+def migrate(): # new 
+   db.evolve(ignore_tables={'base_model'}) # new
 
 @app.route("/")
 def index():
