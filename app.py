02ad74531e6cf9d604ff8fd22d9c9ee681fa82a7
Syakir Wong <mohdsyakir9997@gmail.com>
2021-01-11 15:57:49 +0800

    added form add store to database. fixed databse grants

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import peeweedbevolve
 from flask import Flask, render_template, request
-from models import db
+from models import *
 
 app = Flask(__name__)
 
@@ -20,6 +20,17 @@ def migrate():
 @app.route("/")
 def index():
    return render_template('index.html')
+
+@app.route("/store")
+def add_store():
+   return render_template('store.html')
+
+@app.route("/accept-add-store-request", methods=["POST"])
+def receive_store():
+   store_name = request.form['store_name']
+   store = Store(name = store_name)
+   store.save()
+   return render_template('accept_store.html', store_name=store_name)
 
 if __name__ == '__main__':
    app.run()
