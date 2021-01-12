dad656538168f29f33144edcb3c703e0584a7b8d
Syakir Wong <mohdsyakir9997@gmail.com>
2021-01-12 10:57:30 +0800

    added individual store view

--- app.py
+++ app.py
@@ -23,7 +23,13 @@ def index():
 
 @app.route("/store")
 def add_store():
-   return render_template('store.html')
+   stores = Store.select()
+   return render_template('store.html', stores=stores)
+
+@app.route("/store/<int:id>")
+def view_individual_store(id):
+   store = Store.get_by_id(id)
+   return render_template('view-store.html', store=store)
 
 @app.route("/accept-add-store-request", methods=["POST"])
 def receive_store():
@@ -31,6 +37,19 @@ def receive_store():
    store = Store(name = store_name)
    store.save()
    return render_template('accept_store.html', store_name=store_name)
+
+@app.route("/warehouse")
+def add_warehouse():
+   stores = Store.select()
+   return render_template('warehouse.html', stores = stores)
+
+@app.route("/accept-add-warehouse-request", methods=["POST"])
+def receive_warehouse():
+   store = Store.get_by_id(request.form['store_id'])
+   warehouse_location = request.form['warehouse_location']
+   warehouse = Warehouse(location = warehouse_location, store_id = store)
+   warehouse.save()
+   return render_template('accept_warehouse.html', location = warehouse_location, store = store)
 
 if __name__ == '__main__':
    app.run()
